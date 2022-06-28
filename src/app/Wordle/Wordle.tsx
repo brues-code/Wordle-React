34cc930f54ef25838fe4b263f2d8653f54434604
Skyler H <skylerh15@github.com>
2022-06-27 23:45:57 -0500

    conditionally render keyboard

--- src/app/Wordle/Wordle.tsx
+++ src/app/Wordle/Wordle.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+
+import { useApp } from 'app/context/AppContext'
 
 import AppLayout from 'app/layouts/AppLayout'
 import WordleBoard from 'app/components/WordleBoard'
 import Keyboard from 'app/components/Keyboard'
 
-const Wordle = () => (
-    <AppLayout>
-        <WordleBoard />
-        <Keyboard />
-    </AppLayout>
-)
+const Wordle = () => {
+    const { solutionFound } = useApp()
 
+    const renderKeyBoard = useMemo(
+        () => !solutionFound && <Keyboard />,
+        [solutionFound]
+    )
+
+    return (
+        <AppLayout>
+            <WordleBoard />
+            {renderKeyBoard}
+        </AppLayout>
+    )
+}
 export default Wordle
