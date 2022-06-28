55fd9b92b2364e8aa99c55c930da69f167b6ccd0
Skyler H <skylerh15@github.com>
2022-06-27 23:12:43 -0500

    Add Icons component to use svgs
    added icon_backspace to start

---
+++ src/app/components/Icon/Icon.tsx
@@ -0,0 +1,41 @@
+import React, { FC, Ref, MouseEvent, CSSProperties } from 'react'
+import svg4Everybody from 'svg4everybody'
+
+import { StyledIcon, StyledIconContainer } from './styles'
+
+const icons = require('styles/resources/icons.svg').default
+
+svg4Everybody()
+
+export interface IconComponentProps {
+    className?: string
+    display?: string
+    fill?: string
+    height?: string
+    innerRef?: Ref<HTMLDivElement>
+    name: string
+    onClick?: (e: MouseEvent<HTMLDivElement>) => void
+    onMouseDown?: (e: MouseEvent<HTMLDivElement>) => void
+    style?: CSSProperties
+    title?: string
+    width?: string
+}
+
+const Icon: FC<IconComponentProps> = ({
+    name,
+    width,
+    height,
+    fill,
+    title,
+    innerRef,
+    ...restProps
+}) => (
+    <StyledIconContainer ref={innerRef} {...restProps}>
+        <StyledIcon width={width} height={height} fill={fill || 'currentColor'}>
+            {title && <title>{title}</title>}
+            <use xlinkHref={icons + '#' + name} />
+        </StyledIcon>
+    </StyledIconContainer>
+)
+
+export default Icon
